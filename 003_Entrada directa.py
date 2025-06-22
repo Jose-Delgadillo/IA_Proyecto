7f7a8497b118da92174f5633b3a6c18ed5882cb5
Jose-Delgadillo <a22310205@ceti.mx>
2025-06-22 15:46:17 -0600

    Update 003_Entrada directa.py

--- 003_Entrada directa.py
+++ 003_Entrada directa.py
@@ -2,18 +2,9 @@ import numpy as np
 from PIL import ImageGrab
 import cv2
 import time
-from directkeys import PressKey, W, A, S, D
+from directkeys import PressKey, ReleaseKey, W, A, S, D
 
-# gives us time to get situated in the game
-for i in list(range(4))[::-1]:
-    print(i+1)
-    time.sleep(1)
 
-print('down')
-PressKey(W) 
-time.sleep(3)
-print('up')
-PressKey(W) 
 
 #Imagen procesada con Canny para simplificar bordes
 def process_img(original_image):
@@ -23,20 +14,29 @@ def process_img(original_image):
     processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
     return processed_img
 
+# gives us time to get situated in the game
+for i in list(range(4))[::-1]:
+    print(i+1)
+    time.sleep(1)
 
 
-# last_time = time.time()
-# while(True):
-#     screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-#     new_screen = process_img(screen)
 
-#     print('Loop took {} seconds'.format(time.time()-last_time))
-#     last_time = time.time()
-#     cv2.imshow('window', new_screen)
-#     #cv2.imshow('window2',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
-#     if cv2.waitKey(25) & 0xFF == ord('q'):
-#         cv2.destroyAllWindows()
-#         break
+last_time = time.time()
+while(True):
+    screen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+    wwnew_screen = process_img(screen)
+    print('down')
+    PressKey(W) 
+    time.sleep(3)
+    print('up')
+    ReleaseKey(W) 
+    print('Loop took {} seconds'.format(time.time()-last_time))
+    last_time = time.time()
+    cv2.imshow('window', new_screen)
+    #cv2.imshow('window2',cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 #main()
 #from IPython.display import Image
 #Image(filename='edge-detection.png') 
